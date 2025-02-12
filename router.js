0131a9478b6e87e52b277ef481480e412b3e653f
Hanjo <hanjo0305@gmail.com>
2025-02-13 02:27:53 +0900

    #1 백엔드 준비 완료

---
+++ router.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+function route(pathname, handle, response) {
+    console.log('pathname : ' + pathname);
+
+
+    if (typeof handle[pathname] === 'function') {
+        handle[pathname](response);     
+    } else {
+        response.writeHead(404, {'Content-Type' : 'text/html'});
+        response.write('Not Found');
+        response.end();
+    }
+    
+}
+
+exports.route = route
