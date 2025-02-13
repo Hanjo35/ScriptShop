5dc0c672c8acf257d547e2ad895c43bb3e02fe53
Hanjo <hanjo0305@gmail.com>
2025-02-13 14:41:31 +0900

    #1 메인 페이지 연동 및 요소 가로 정렬

--- requestHandler.js
+++ requestHandler.js
@@ -1,28 +1,64 @@
+const fs = require('fs');
+const main_view = fs.readFileSync('./main.html');
+
 const mariadb = require('./database/connect/mariadb');
 
 function main(response) {
     console.log('main');
 
-    mariadb.query("SELECT * FROM product", function(err, rows) {
+    mariadb.query("SELECT * FROM product", function(err, rows) { 
         console.log(rows);
     })
 
     response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('Main Page');
+    response.write(main_view);
     response.end();
     
 }
 
-function login(response) {
-    console.log('login');
+function redRacket(response) {
+    fs.readFile('./img/redRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
 
+function blueRacket(response) {
+    fs.readFile('./img/blueRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+
+function blackRacket(response) {
+    fs.readFile('./img/blackRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+
+function order(response, productId) {
     response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('Login Page');
+
+    mariadb.query("INSERT INTO orderlist VALUES (" + productId + ", '" + new Date().toLocaleDateString() + "');" ,function(err, rows) { 
+        console.log(rows);
+    })
+
+    response.write('order page');
     response.end();
 }
 
 let handle = {}; // key:value
+
 handle['/'] = main;
-handle['/login'] = login;
+handle['/order'] = order;
+
+/** image directory **/
+handle['/img/redRacket.png'] = redRacket;
+handle['/img/blueRacket.png'] = blueRacket;
+handle['/img/blackRacket.png'] = blackRacket;
 
 exports.handle = handle;
--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
-function route(pathname, handle, response) {
+function route(pathname, handle, response, productId) {
     console.log('pathname : ' + pathname);
 
 
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response);     
+        handle[pathname](response, productId);     
     } else {
         response.writeHead(404, {'Content-Type' : 'text/html'});
         response.write('Not Found');
